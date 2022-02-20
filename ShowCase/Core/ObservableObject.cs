56c148af53333d17f061a357b62cfbc4a1903dc7
williamskei97 <williamskei97@gmail.com>
2022-02-20 17:10:12 -0500

    Adding tools to Showcase to allow for easier viewing of new control states

---
+++ ShowCase/Core/ObservableObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ShowCase.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
--- ShowCase/ViewModels/ButtonsViewModel.cs
+++ ShowCase/ViewModels/ButtonsViewModel.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ShowCase.Core;
 
 namespace ShowCase.ViewModels
 {
-    public class ButtonsViewModel
+    public class ButtonsViewModel : ObservableObject
     {
+
+        private bool isActivated;
+        public bool IsActivated
+        {
+            get { return isActivated; }
+            set
+            {
+                if(value != isActivated)
+                {
+                    isActivated = value;
+                    OnPropertyChanged(nameof(IsActivated));
+                }
+            }
+        }
+
+        public ButtonsViewModel()
+        {
+            IsActivated = false;
+        }
 
     }
 }
