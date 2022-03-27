d85805927821df55f54a47491bc885e918f559bd
williamskei97 <williamskei97@gmail.com>
2022-03-27 16:01:47 -0400

    Added a new CustomTextBox type to toolkit that allows setting the default text to something other than "Search"

---
+++ FlatUITheme/src/CustomTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FlatUITheme.src
+{
+    public class CustomTextBox : TextBox
+    {
+        public static readonly DependencyProperty DefaultTextProperty = DependencyProperty.Register(
+            name: "DefaultText",
+            propertyType: typeof(string),
+            ownerType: typeof(CustomTextBox),
+            typeMetadata: new FrameworkPropertyMetadata(defaultValue: "Search"));
+        public string DefaultText
+        {
+            get { return (string)GetValue(DefaultTextProperty); }
+            set { SetValue(DefaultTextProperty, value); }
+        }
+    }
+}
