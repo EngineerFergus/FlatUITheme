31a538756a7ca2b8138878100d5337dba5932abf
williamskei97 <williamskei97@gmail.com>
2022-03-27 17:14:40 -0400

    Updated functionality of Textboxes view and added new ViewModel

---
+++ ShowCase/ViewModels/TextBoxesViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowCase.ViewModels
+{
+    public class TextBoxesViewModel : Core.ObservableObject
+    {
+        private bool isActivated;
+        public bool IsActivated
+        {
+            get { return isActivated; }
+            set
+            {
+                if(value != isActivated)
+                {
+                    isActivated = value;
+                    OnPropertyChanged(nameof(IsActivated));
+                }
+            }
+        }
+
+        public TextBoxesViewModel()
+        {
+            IsActivated = true;
+        }
+    }
+}
